462911a0929e6056c8de5252ceee9db256c52e6b
Honda <honda.muneer@gmail.com>
2024-08-10 17:57:32 +0300

    adding models

--- car_license/base/models.py
+++ car_license/base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
-
 # Create your models here.
 
 class CarDetails (models.Model):
@@ -16,3 +16,11 @@ class Reservation (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     car = models.ForeignKey(CarDetails, on_delete=models.CASCADE)
     date = models.DateTimeField()
+
+
+class UserInfo (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50)
+    national_id = models.CharField(max_length=50, unique=True)
+    number = models.IntegerField(unique=True)
+    address = models.CharField(unique=True, max_length=50)
