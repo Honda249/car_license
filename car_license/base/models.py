1036c53955dfad17f76d010a69912a76831939a4
Honda <honda.muneer@gmail.com>
2024-08-10 14:13:44 +0300

    adding Models

--- car_license/base/models.py
+++ car_license/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class CarDetails (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    make  = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    chassis = models.CharField(max_length=50, unique=True)
+    eng_num = models.CharField(max_length=50, unique=True)
+    year  = models.IntegerField()
+    
+class Reservation (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(CarDetails, on_delete=models.CASCADE)
+    date = models.DateTimeField()
