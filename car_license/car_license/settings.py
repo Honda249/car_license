bf4a7fd7a5f15dacbede079ca47352bc74da2c1e
Honda <honda.muneer@gmail.com>
2024-08-11 17:39:04 +0300

    adding templates

--- car_license/car_license/settings.py
+++ car_license/car_license/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'car_license.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
