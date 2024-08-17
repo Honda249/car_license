8be135038f1e2acdc04aeb53b9aa52bde54af648
Honda <honda.muneer@gmail.com>
2024-08-17 22:28:37 +0300

    updating temolates and adding index view

--- car_license/base/views/indexView.py
+++ car_license/base/views/indexView.py
@@ -5,5 +5,11 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomePageView(TemplateView):
-    template_name = 'base/index.html'
-    
+    template_name = 'base/index.html'  # Specify the template you want to render
+
+    # Optionally, you can override the get_context_data method to pass context to the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home Page'  # Example of adding a variable to the context
+        return context
+
